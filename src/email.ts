d44bcb9f6bc5dba71658608afbb3395637c0fb62
Hashib Raja <108208385+hashib786@users.noreply.github.com>
2023-08-26 15:51:00 +0000

    completed api for sending mail me and also feedback message who message me

--- src/email.ts
+++ src/email.ts
@@ -9,3 +9,14 @@ const transporter = nodemailer.createTransport({
       pass: process.env.GEMAIL_PASSWORD,
     },
   });
+
+const sendMail = async (email : string, subject : string, html : string) =>{
+    const info = await transporter.sendMail({
+        from: `Hashib Raja <${process.env.EMAIL_FROM}>`, // sender address
+        to: email, // list of receivers
+        subject, // Subject line
+        html, // html body
+      });
+}
+
+export default sendMail;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
-import express, { NextFunction, Request, Response } from "express"
 import dontenv from "dotenv"
 dontenv.config()
 
+import fs from "fs"
+import { join } from "path"
+import express, { NextFunction, Request, Response } from "express"
+import sendMail from "./email"
+
+
 const app = express()
 
-app.post("/sendMail", async (req : Request, res : Response, next : NextFunction) =>{
+app.use(express.json())
 
+const currentWorkingDirectory = process.cwd()
+
+const gettingMessageHTML = fs.readFileSync(join(currentWorkingDirectory, "./template/gettingMessage.html"), "utf-8")
+const recievedMessageHTML = fs.readFileSync(join(currentWorkingDirectory, "./template/recievedMessage.html"), "utf-8")
+
+
+app.post("/sendMail", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, subject, name, message } = req.body;
+
+        let afterchangeGettingMessage = gettingMessageHTML.replace(/\{%NAME%}/g, name);
+        afterchangeGettingMessage = afterchangeGettingMessage.replace(/\{%EMAIL%}/g, email);
+        afterchangeGettingMessage = afterchangeGettingMessage.replace(/\{%SUBJECT%}/g, subject);
+        afterchangeGettingMessage = afterchangeGettingMessage.replace(/\{%MESSAGE%}/g, message);
+
+
+        //    This is sending me my mail 
+        await sendMail(process.env.GEMAIL_MY!, subject, afterchangeGettingMessage)
+        await sendMail(email, "Message Received", recievedMessageHTML)
+
+        res.status(200).json({
+            status: "success",
+            "message": "Message sended Successfully"
+        })
+    } catch (error) {
+        res.status(403).json({
+            status: "fail",
+            "message": error || "someting is wrong sending Mail"
+        })
+    }
+})
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        status: "fail",
+        "message": "This route is not defined"
+    })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on " + process.env.PORT || 3000)
 })
